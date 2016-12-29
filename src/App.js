3c22bec54896d085b0e4e9598ce9ba84321a0096
Joe Komskis <Joe Komskis>
2016-12-29 16:07:32 -0500

    Improved mobile layout.

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ class App extends Component {
           </Col>
         </row>
         <row>
-          <Col id="langlist" xs={11}>
+          <Col id="langlist" xs={10} sm={11}>
             <LangList/>
           </Col>
-          <Col xs={1}>
+          <Col xs={2} sm={1}>
             <AddLangBoxButton/>
           </Col>
         </row>
@@ -42,7 +42,11 @@ class App extends Component {
             <TranslateButton/>
           </Col>
         </row>
-        <hr className="hr"/>
+        <row>
+        <Col xs={12}>
+          <hr className="hr"/>
+          </Col>
+        </row>
         <row>
           <Col xs={12} sm={6} className="textBox">
             <p>Input Text:</p>
@@ -191,7 +195,7 @@ class LangBox extends React.Component {
 
   render() {
     return(
-      <Col xs={2} style={{padding:'1%'}}>
+      <Col xs={6} sm={2} style={{padding:'1%'}}>
         <FormControl id={"LangBox"+this.props.index} className="LangBox" componentClass="select" value={this.props.language} onChange={this.handleChange.bind(this)}>
         <option value="af">Afrikaans</option><option value="sq">Albanian</option><option value="ar">Arabic</option><option value="az">Azerbaijani</option><option value="eu">Basque</option><option value="bn">Bengali</option><option value="be">Belarusian</option><option value="bg">Bulgarian</option><option value="ca">Catalan</option><option value="zh-CN">Chinese Simplified</option><option value="zh-TW">Chinese Traditional</option><option value="hr">Croatian</option><option value="cs">Czech</option><option value="da">Danish</option><option value="nl">Dutch</option><option value="en">English</option><option value="eo">Esperanto</option><option value="et">Estonian</option><option value="tl">Filipino</option><option value="fi">Finnish</option><option value="fr">French</option><option value="gl">Galician</option><option value="ka">Georgian</option><option value="de">German</option><option value="el">Greek</option><option value="gu">Gujarati</option><option value="ht">Haitian Creole</option><option value="iw">Hebrew</option><option value="hi">Hindi</option><option value="hu">Hungarian</option><option value="is">Icelandic</option><option value="">Indonesian</option><option value="ga">Irish</option><option value="it">Italian</option><option value="ja">Japanese</option><option value="kn">Kannada</option><option value="ko">Korean</option><option value="la">Latin</option><option value="lv">Latvian</option><option value="lt">Lithuanian</option><option value="mk">Macedonian</option><option value="ms">Malay</option><option value="mt">Maltese</option><option value="no">Norwegian</option><option value="fa">Persian</option><option value="pl">Polish</option><option value="pt">Portuguese</option><option value="ro">Romanian</option><option value="ru">Russian</option><option value="sr">Serbian</option><option value="sk">Slovak</option><option value="sl">Slovenian</option><option value="es">Spanish</option><option value="sw">Swahili</option><option value="sv">Swedish</option><option value="ta">Tamil</option><option value="te">Telugu</option><option value="th">Thai</option><option value="tr">Turkish</option><option value="uk">Ukrainian</option><option value="ur">Urdu</option><option value="vi">Vietnamese</option><option value="cy">Welsh</option><option value="yi">Yiddish</option>
         </FormControl>
